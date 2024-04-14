9e83dc104dc978842ea886d3845497fb85674c08
dk10119 <tranduclong092@gmail.com>
2024-04-14 14:30:39 +0300

    complete the index page, add some sample data to user table

--- phpDir/src/index.php
+++ phpDir/src/index.php
@@ -26,13 +26,46 @@
             </div>
 
             <div class="main">
-                <p>main</p>
+                <h2>Welcome to the registration website</h2>
+                <p>In this website, you can register an account and login</p>
+
+                <h2 class="mt_20 mb_20">Registered users</h2>
+                <table class="t1">
+                    <tr>
+                        <th>ID</th>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Alex</td>
+                        <td>Tran</td>
+                        <td>alex@example.com</td>
+                        <td>111111</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Matt</td>
+                        <td>Jones</td>
+                        <td>matt@example.com</td>
+                        <td>222222</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Vera</td>
+                        <td>Smith</td>
+                        <td>vera@example.com</td>
+                        <td>3333333</td>
+                    </tr>
+                </table>
             </div>
 
-            <div class="copyright mt_10">
-                <p>Copyright &copy; Alex Tran</p>
-            </div>
 
+        </div>
+        <div class="copyright mt_10">
+            <p>Copyright &copy; Alex Tran</p>
         </div>
     </div>
 
