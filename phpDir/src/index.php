d69ad496b270e7aa33fd0f8b42ecdbc3413115fc
dk10119 <tranduclong092@gmail.com>
2024-04-14 14:24:46 +0300

    create simple index page with placeholder nav links and main section

--- phpDir/src/index.php
+++ phpDir/src/index.php
@@ -3,13 +3,39 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Authentication System</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <h1>Hello world!</h1>
-    <?= phpinfo() ?>
+
+    <div class="wrapper">
+        <div class="container">
+
+            <div class="nav">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">Registration</a></li>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+
+            <div class="main">
+                <p>main</p>
+            </div>
+
+            <div class="copyright mt_10">
+                <p>Copyright &copy; Alex Tran</p>
+            </div>
+
+        </div>
+    </div>
+
 </body>
 
 </html>
