e7d756b993cd5fc6e3380ac12e3f7b9d493c02fe
dk10119 <tranduclong092@gmail.com>
2024-04-15 17:01:58 +0300

    add redirect rule to dashboard and login page, add input check on login page

--- phpDir/src/login.php
+++ phpDir/src/login.php
@@ -1,6 +1,35 @@
-<?php include './components/header.php'; ?>
+<?php
+if (isset($_SESSION['user'])) {
+    include_once('./components/config.php');
+    header('Location: ' . BASE_URL);
+    exit();
+}
+
+include_once './components/header.php';
+
+if (isset($_POST['login_form'])) {
+    try {
+        if ($email == '') {
+            throw new Exception('Email cannot be empty');
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception('Invalid email format (Example@example.com)');
+        }
+
+        if ($pwd == '') {
+            throw new Exception('Password cannot be empty');
+        }
+    } catch (Exception $e) {
+        $error_msg = $e->getMessage();
+    }
+}
+?>
 
 <h2 class="mb_10">Login</h2>
+
+<?php if (isset($error_msg)) : ?>
+    <p class='error'><?= $error_msg ?></p>
+<?php endif; ?>
+
 <form action="" method="post">
     <table class="t2">
         <tr>
